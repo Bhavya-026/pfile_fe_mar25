5d33e3bc29c796052a18ca13ab83104d86f991f4
Bhavya Jadhav <divyajadhav026@gmail.com>
2025-03-18 15:56:55 +0530

    this is changes files

---
+++ js_programs/p2.js
@@ -0,0 +1,7 @@
+let numbers = [10, 20, 30, 40, 50];
+ 
+let quotientsOf5 = numbers.map((arrayElement) => {
+    return arrayElement / 5;
+})
+console.log(quotientsOf5)
+
---
+++ js_programs/p3.js
@@ -0,0 +1,10 @@
+let numbers = [10, 20, 30, 40, 50];
+ 
+
+let lessThan30 = numbers.filter((e) => 
+    {
+        return e < 30;
+    }
+)
+console.log(lessThan30)
+
